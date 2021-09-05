56012f9bb7e10320a7db70aaa0230ea5834f29cc
Anna Sapegina <watochka@gmail.com>
2021-09-05 20:31:00 +0300

    feat: implement #1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
+    min
+    max
+    lastGuess
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let x = (this.min + this.max) / 2;
+        this.lastGuess = Math.round(x)
+        return this.lastGuess
     }
 
     lower() {
-
+        if (this.max <= this.min + 1) {
+            return
+        }
+        this.max = this.lastGuess - 1
     }
 
     greater() {
-
+        if (this.max <= this.min + 1) {
+            return
+        }
+        this.min = this.lastGuess + 1
     }
 }
 
